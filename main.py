9d410066f961d6b02f22185512176841715bf5c3
YuliaCheprasova <cheprasovaja@yandex.ru>
2023-04-29 09:42:06 +0700

    the first task is done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math as m
+import matplotlib.pyplot as plt
 
 
 def Y(x):
@@ -20,47 +21,55 @@ def K(z):
 
 def Ksum(z0, z, n):
     sum=0
+    hn = H(n)
     for i in range (len(z)):
-        sum+=K((z0-z[i])/H(n))
+        sum+=K((z0-z[i])/hn)
     return sum
 
 
 def H(n):
-    c=1
+    c=68
     h=c*n**(-1/5)
     return h
 
 
 def M(x0, x, y, n):
     sum = 0
+    ksum = Ksum(x0, x, n)
     for i in range(len(x)):
-        sum += K((x0-x[i])/H(n))/Ksum(x0, x, n)*y[i]
+
+        sum += K((x0-x[i])/H(n))/ksum*y[i]
     return sum
 
 
-def delete(j, x):
-    xwithoutj = np.zeros(99)
+def delete(j, x, n):
+    xwithoutj = np.zeros(n-1)
     for i in range (j):
        xwithoutj[i] = x[i]
-    for i in range (j+1, len(x)-1):
-        xwithoutj[i] = x[i-1]
+    for i in range (j, len(x)-1):
+        xwithoutj[i] = x[i+1]
     return xwithoutj
 
 
 if __name__ == '__main__':
-    n=100
-    x = np.zeros(100)
-    for i in range(1,101):
-        x[i-1]=i
+    n=1000
+    x = np.zeros(n)
+    for i in range(1,n+1):
+        x[i-1]=i*0.1
     y=Y(x)
-    print("y = ", y)
-    j=55
-    x0=x[j]
-    xwithoutj = delete(j, x)
-    y0=y[j]
-    ywithoutj = delete(j, y)
-    R = M(x0, xwithoutj, ywithoutj, n)
-    print(R)
+    #print("y = ", y)
+    #plt.plot(x,y)
+    #plt.show()
+    ypredict = np.zeros(n)
+    E = np.zeros(n)
+    for j in range(n):
+        x0=x[j]
+        xwithoutj = delete(j, x, n)
+        y0=y[j]
+        ywithoutj = delete(j, y, n)
+        ypredict[j] = M(x0, xwithoutj, ywithoutj, n)
+        E[j] = (ypredict[j]-y0)**2
+    #plt.plot(x,ypredict)
+    #plt.show()
+    print(sum(E))
 
-# правильно ли я понимаю что нужно исключить каждую из точек
-# и посчитать среднеквадратическую ошибку, а потом сложить эти ошибки?
